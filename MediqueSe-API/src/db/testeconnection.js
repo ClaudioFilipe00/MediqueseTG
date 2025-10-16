0b4264dea838480ddfcb589395511f975e6206f0
Claudio <fih198@hotmail.com>
2025-10-16 12:42:26 -0300

    att alarme

--- MediqueSe-API/src/db/testeconnection.js
+++
@@ -1,5 +0,0 @@
-import { sequelize } from "./connection.js";
-
-sequelize.authenticate()
-  .then(() => console.log("✅ Conectado ao Neon com sucesso!"))
-  .catch(err => console.error("❌ Erro de conexão:", err));
--- MediqueSe-API/src/routes/pushRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { registerPushToken } from "../controllers/pushController.js";
-const router = express.Router();
-
-router.post("/register", registerPushToken);
-
-export default router;
