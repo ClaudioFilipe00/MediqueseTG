e0501812c2163207fac17f42cecabe1b73bd1218
Claudio <fih198@hotmail.com>
2025-10-22 16:06:21 -0300

    services att

--- MediqueSe-API/src/services/notificacaoService.js
+++ MediqueSe-API/src/services/notificacaoService.js
@@ -1,5 +1,6 @@
 import admin from "firebase-admin";
-import serviceAccount from "../firebase-service-account.json" assert { type: "json" };
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -7,37 +8,4 @@ if (!admin.apps.length) {
   });
 }
 
-export async function enviarNotificacao(token, titulo, corpo) {
-  const message = {
-    notification: {
-      title: titulo,
-      body: corpo,
-    },
-    token,
-  };
-
-  try {
-    const response = await admin.messaging().send(message);
-    console.log("Notificação enviada com sucesso:", response);
-    return response;
-  } catch (error) {
-    console.error("Erro ao enviar notificação:", error);
-    throw error;
-  }
-}
-
-export async function enviarNotificacaoTodos(tokens, titulo, corpo) {
-  const messages = tokens.map((token) => ({
-    notification: { title: titulo, body: corpo },
-    token,
-  }));
-
-  try {
-    const response = await admin.messaging().sendAll(messages);
-    console.log("Notificações enviadas com sucesso:", response.successCount);
-    return response;
-  } catch (error) {
-    console.error("Erro ao enviar notificações:", error);
-    throw error;
-  }
-}
+export default admin;
