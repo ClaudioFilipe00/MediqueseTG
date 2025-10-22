83210dc85b4cf42937f5bb30352af408a75e3d49
Claudio <fih198@hotmail.com>
2025-10-22 12:18:01 -0300

    Att login 2

--- MediqueSe-API/src/controllers/usuarioController.js
+++ MediqueSe-API/src/controllers/usuarioController.js
@@ -1,6 +1,11 @@
 import { Usuario } from "../models/usuarioModel.js";
 import { Medicamento } from "../models/medicamentoModel.js";
 
+// Função para converter DD/MM/YYYY -> YYYY-MM-DD
+const formatarDataParaBD = (data) => {
+  const [dia, mes, ano] = data.split("/");
+  return `${ano}-${mes}-${dia}`;
+};
 
 export const criarUsuario = async (req, res) => {
   console.log("REQ BODY:", req.body);
@@ -23,21 +28,21 @@ export const criarUsuario = async (req, res) => {
 
 export const loginUsuario = async (req, res) => {
   try {
-    const { telefone, data_nascimento } = req.body;
+    let { telefone, data_nascimento } = req.body;
+
+    if (!telefone || !data_nascimento)
+      return res.status(400).json({ erro: "Telefone e data de nascimento são obrigatórios." });
+
+    data_nascimento = formatarDataParaBD(data_nascimento);
 
     const usuario = await Usuario.findOne({
       where: { telefone, data_nascimento },
-      include: [
-        {
-          model: Medicamento,
-          as: "medicamentos",
-        },
-      ],
+      include: [{ model: Medicamento, as: "medicamentos" }],
       attributes: ["id", "nome", "telefone", "data_nascimento"],
     });
 
     if (!usuario) {
-      return res.status(404).json({ erro: "Usuário não encontrado" });
+      return res.status(401).json({ erro: "Erro ao efetuar login. Verifique suas credenciais." });
     }
 
     return res.status(200).json({
@@ -52,7 +57,6 @@ export const loginUsuario = async (req, res) => {
     return res.status(500).json({ erro: "Erro ao efetuar login" });
   }
 };
-
 
 export const atualizarUsuario = async (req, res) => {
   try {
