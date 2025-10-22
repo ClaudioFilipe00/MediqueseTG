c336d28dd9d83895b5cc718be48c75a7df9773bf
Claudio <fih198@hotmail.com>
2025-10-22 11:41:55 -0300

    Att login

--- MediqueSe-API/src/routes/usuarioRoutes.js
+++ MediqueSe-API/src/routes/usuarioRoutes.js
@@ -4,9 +4,8 @@ import * as usuarioCtrl from "../controllers/usuarioController.js";
 const router = express.Router();
 
 router.post("/", usuarioCtrl.criarUsuario);
-router.get("/:telefone", usuarioCtrl.obterUsuarioPorTelefone);
+router.post("/login", usuarioCtrl.loginUsuario);
 router.put("/:id", usuarioCtrl.atualizarUsuario);
 router.delete("/:id", usuarioCtrl.excluirUsuario);
-router.post("/login", usuarioCtrl.loginUsuario);
 
 export default router;
